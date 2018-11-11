a2ae4875ea47873d8015101aef1b6c2e208cb068
Emile Fugulin <emilefugulin@hotmail.com>
2018-11-11 00:05:32 -0500

    Add news feed reader

--- src/main/kotlin/ca/usherbrooke/degel/controllers/FeedsController.kt
+++ src/main/kotlin/ca/usherbrooke/degel/controllers/FeedsController.kt
@@ -3,17 +3,29 @@ package ca.usherbrooke.degel.controllers
 import ca.usherbrooke.degel.config.Constants.API
 import ca.usherbrooke.degel.config.Permissions
 import ca.usherbrooke.degel.models.Feed
+import ca.usherbrooke.degel.models.News
 import ca.usherbrooke.degel.services.FeedsService
+import mu.KLogging
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping(API)
 class FeedsController(val feedsService: FeedsService) {
+    companion object: KLogging()
+
     @GetMapping("/feeds")
     fun getFeeds() = feedsService.getFeeds()
 
     @PreAuthorize(Permissions.HAS_ADMIN_ROLE)
     @PostMapping("/feeds")
     fun upsertFeed(@RequestBody feed: Feed) = feedsService.upsertFeed(feed)
+
+    @PreAuthorize("${Permissions.USER_OWN_RESSOURCE} or ${Permissions.HAS_ADMIN_ROLE}")
+    @GetMapping("/user/{id}/news")
+    fun getNews(@PathVariable id: UUID) : List<News> {
+        logger.debug("User $id requests his news")
+        return feedsService.getNews(id)
+    }
 }
--- src/main/kotlin/ca/usherbrooke/degel/services/FeedsService.kt
+++ src/main/kotlin/ca/usherbrooke/degel/services/FeedsService.kt
@@ -2,20 +2,63 @@ package ca.usherbrooke.degel.services
 
 import ca.usherbrooke.degel.entities.FeedEntity
 import ca.usherbrooke.degel.models.Feed
+import ca.usherbrooke.degel.models.News
 import ca.usherbrooke.degel.repositories.FeedsRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.client.RestTemplate
+import java.util.*
+import com.rometools.rome.io.SyndFeedInput
+import com.rometools.rome.io.XmlReader
+import org.apache.commons.lang.StringEscapeUtils
+import org.jsoup.Jsoup
+import org.jsoup.safety.Whitelist
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.Resource
+import java.net.URL
+
 
 interface FeedsService {
     fun getFeeds(): List<Feed>
     fun upsertFeed(feed: Feed): Feed
+    fun getNews(userId: UUID) : List<News>
 }
 
 @Service
-class FeedsServiceImpl(private val feedsRepository: FeedsRepository) : FeedsService {
+class FeedsServiceImpl(
+        private val feedsRepository: FeedsRepository,
+        private val settingsService: SettingsService,
+        @Value("\${app.feeds.max-news-elements}") val maxNewsElements: Int
+) : FeedsService {
     @Transactional
     override fun getFeeds(): List<Feed> {
         return feedsRepository.findAll().map { it -> it.toModel() }
+    }
+
+    @Transactional
+    override fun getNews(userId: UUID) : List<News> {
+        val settings = settingsService.getSettings(userId)
+        val feeds = feedsRepository.findAllById(settings.feeds)
+
+        val client = RestTemplate()
+        val input = SyndFeedInput()
+        return feeds.map {
+            val response = client.getForEntity(it.url, Resource::class.java)
+            val feed = input.build(XmlReader(response.body!!.inputStream))
+            val baseUrl = URL(it.url)
+            feed.entries.map {
+                val description = Jsoup.clean(it.description.value, Whitelist.none())
+                News(
+                        it.title,
+                        StringEscapeUtils.unescapeHtml(description),
+                        "${baseUrl.protocol}://${baseUrl.authority}${it.link}",
+                        it.enclosures.getOrNull(0)?.url ?: "",
+                        it.publishedDate
+                )
+            }
+        }.flatten().sortedByDescending {
+            it.publishedDate
+        }.take(maxNewsElements)
     }
 
     @Transactional
